40565f700365f485e24138740b047dd474a90f26
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-02-09 17:49:34 -0300

    adding classes

---
+++ ListaDois.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace cs;
+
+public class ListaDois
+{
+
+    static void main(string [] args)
+    {
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
